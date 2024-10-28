2198e66681fef1cb15d5987845266580a503f857
Ibai Perez <perez.ibai@uni.eus>
2024-10-28 10:27:39 +0100

    controllerrak

--- src/main/java/dambat/controller/NombreController.java
+++ src/main/java/dambat/controller/NombreController.java
@@ -1,13 +1,14 @@
 package dambat.controller;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
-import javafx.scene.control.Button;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import javafx.fxml.FXMLLoader;
-import java.io.IOException;
 
 public class NombreController {
 
