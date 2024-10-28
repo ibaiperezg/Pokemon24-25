ad891ee77336b54902ccd1a37102d1cbc457efb5
Ibai Perez <perez.ibai@uni.eus>
2024-10-28 22:16:56 +0100

    HASI BOTOIA EZ DOA

--- src/main/java/dambat/controller/NombreController.java
+++ src/main/java/dambat/controller/NombreController.java
@@ -1,7 +1,6 @@
 package dambat.controller;
 
 import java.io.IOException;
-
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -23,22 +22,26 @@ public class NombreController {
         String nombre = nombreField.getText();
 
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/jolasa.fxml"));
+            // Cargar la nueva escena
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/dambat/Jolasa.fxml")); // Use absolute path if necessary
             Parent root = loader.load();
 
+            // Obtener el controlador de Jolasa
             JolasaController jolasaController = loader.getController();
             jolasaController.setNombre(nombre); // Pasar el nombre
 
+            // Obtener el escenario actual
             Stage stage = (Stage) guardarButton.getScene().getWindow();
-            stage.setScene(new Scene(root, 300, 200));
+            // Cambiar la escena
+            stage.setScene(new Scene(root));
             stage.setTitle("Jolasa");
             stage.show();
         } catch (IOException e) {
-            e.printStackTrace();
+            e.printStackTrace(); // Manejo de errores
         }
     }
 
-    public void movePikachu(double moveDelta, double d) {
+    public void movePikachu(double moveDelta2, double moveDelta) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'movePikachu'");
     }
